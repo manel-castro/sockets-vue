3f649bff5c5be3b5042f8d512df95b67c90ece23
mane936 <53373350+mane936@users.noreply.github.com>
2023-06-06 20:53:56 +0200

    add input

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,30 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main></main>
+  <main>
+    Hello world
+    <select-username @input="onUsernameSelection" />
+  </main>
 </template>
+
+<script lang="ts">
+import SelectUsername from '../components/SelectUsername.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SelectUsername
+  },
+  data() {
+    return {
+      usernameAlreadySelected: false
+    }
+  },
+  methods: {
+    onUsernameSelection(username: string) {
+      this.usernameAlreadySelected = true
+      console.log(username)
+    }
+  }
+}
+</script>
