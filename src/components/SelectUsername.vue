7c3e3ed6fc09e81966840822770dfe136f045cc3
mane936 <53373350+mane936@users.noreply.github.com>
2023-06-06 20:47:30 +0200

    boilerplate

---
+++ src/components/SelectUsername.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <form @submit.prevent="onSubmit">
+      <input v-model="username" placeholder="Your username..." type="text" />
+      <button :disabled="!isValid">Send</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'SelectUsername',
+  data() {
+    return {
+      username: ''
+    }
+  },
+  computed: {
+    isValid() {
+      return this.username.length > 2
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$emit('input', this.username)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
